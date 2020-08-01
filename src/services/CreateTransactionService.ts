d246d30253c30de8666748a3deec9198f56ab830
pyrobit <3159587+pyrobit@users.noreply.github.com>
2020-08-01 14:39:14 -0300

    create and transactions with balance

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,11 +1,32 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
-class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
-  }
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
 }
 
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    type, // category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+    return transaction;
+  }
+}
 export default CreateTransactionService;

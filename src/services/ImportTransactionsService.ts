60a9aac464edce6291b3a609fb5ba2a63cfa1d82
pyrobit <3159587+pyrobit@users.noreply.github.com>
2020-08-02 03:57:19 -0300

    Challenge 6 import resolved

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,88 @@
+import { getCustomRepository, getRepository, In } from 'typeorm';
+import csvParse from 'csv-parse';
+import fs from 'fs';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface TransactionCSV {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionsReadStream = fs.createReadStream(filePath);
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const csvParser = csvParse({
+      delimiter: ',',
+      from_line: 2, // ignore header
+    });
+    const parsedCsv = transactionsReadStream.pipe(csvParser);
+
+    const transactions: TransactionCSV[] = [];
+    const categories: string[] = [];
+
+    parsedCsv.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) {
+        // category is nullable
+        return;
+      }
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+    await new Promise(resolve => parsedCsv.on('end', resolve));
+
+    // check business rule if category exists
+    const existingCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existingCategoriesTitle = existingCategories.map(
+      (category: Category) => category.title,
+    );
+
+    // removes duplicate categories
+    const addCategoryTitles = categories
+      .filter(category => !existingCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoriesRepository.create(
+      addCategoryTitles.map(title => ({
+        title,
+      })),
+    );
+
+    await categoriesRepository.save(newCategories);
+
+    const finalCategories = [...newCategories, ...existingCategories];
+
+    const createdTransactions = transactionsRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionsRepository.save(createdTransactions);
+
+    // exclude file
+    await fs.promises.unlink(filePath);
+    return createdTransactions;
   }
 }
 
